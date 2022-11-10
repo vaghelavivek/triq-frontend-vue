be12b08c26e51e221f9fa6bc06facaf7beac45d8
vaghelavivek150 <vaghelavivek150@gmail.com>
2022-11-10 11:50:06 +0530

    firsr commit

---
+++ src/views/pages/user/Add.vue
@@ -0,0 +1,276 @@
+<script>
+import Layout from "../../../layouts/main.vue";
+import PageHeader from "@/components/page-header";
+
+import "prismjs";
+import "prismjs/themes/prism.css";
+
+export default {
+  data() {
+    return {
+      title: "Add User",
+    };
+  },
+  components: {
+    Layout,
+    PageHeader,
+  },
+  mounted() {},
+};
+</script>
+
+<template>
+  <Layout>
+    <PageHeader :title="title" />
+    <div class="row">
+      <div class="col-xl-12">
+        <div class="card">
+          <div class="card-header align-items-center d-flex">
+            <h4 class="card-title mb-0 flex-grow-1">Add User</h4>
+          </div>
+          <!-- end card header -->
+
+          <div class="card-body">
+            <div class="row">
+              <div class="col-6">
+                <div class="row mb-4">
+                  <div class="col-lg-3">
+                    <label for="nameInput" class="form-label">Name</label>
+                  </div>
+                  <div class="col-lg-9">
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="nameInput"
+                      placeholder="Enter your name"
+                    />
+                  </div>
+                </div>
+                <div class="row mb-4">
+                  <div class="col-lg-3">
+                    <label for="leaveemails" class="form-label">Email</label>
+                  </div>
+                  <div class="col-lg-9">
+                    <input
+                      type="email"
+                      class="form-control"
+                      id="leaveemails"
+                      placeholder="Enter your email"
+                    />
+                  </div>
+                </div>
+                <div class="row mb-4">
+                  <div class="col-lg-3">
+                    <label for="phone" class="form-label">Phone</label>
+                  </div>
+                  <div class="col-lg-9">
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="phone"
+                      placeholder="Enter your phone"
+                    />
+                  </div>
+                </div>
+
+                <div class="row mb-4">
+                  <div class="col-lg-3">
+                    <label for="phone" class="form-label">Language</label>
+                  </div>
+                  <div class="col-lg-9">
+                    <div class="mt-4 mt-lg-0">
+                      <div class="form-check form-check-inline">
+                        <input
+                          class="form-check-input"
+                          type="radio"
+                          name="inlineRadioOptions1"
+                          id="lang-english"
+                          value="english"
+                        />
+                        <label class="form-check-label" for="lang-english"
+                          >English</label
+                        >
+                      </div>
+                      <div class="form-check form-check-inline">
+                        <input
+                          class="form-check-input"
+                          type="radio"
+                          name="inlineRadioOptions2"
+                          id="lang-arabic"
+                          value="arabic"
+                        />
+                        <label class="form-check-label" for="lang-arabic"
+                          >Arabic</label
+                        >
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="row mb-4">
+                  <div class="col-lg-3">
+                    <label for="password" class="form-label">Password</label>
+                  </div>
+                  <div class="col-lg-9">
+                    <input
+                      type="password"
+                      class="form-control"
+                      id="password"
+                      placeholder="Enter your Password"
+                    />
+                  </div>
+                </div>
+
+                <div class="row mb-4">
+                  <div class="col-lg-3">
+                    <label for="business_name" class="form-label"
+                      >Business Name</label
+                    >
+                  </div>
+                  <div class="col-lg-9">
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="business_name"
+                      placeholder="Enter Business Name"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-6">
+               
+                <div class="row mb-4">
+                  <div class="col-lg-3">
+                    <label for="phone" class="form-label">Role</label>
+                  </div>
+                  <div class="col-lg-9">
+                    <div class="mt-4 mt-lg-0">
+                      <div class="form-check form-check-inline">
+                        <input
+                          class="form-check-input"
+                          type="radio"
+                          name="inlineRadioOptions1"
+                          id="super-admin"
+                          value="super-admin"
+                        />
+                        <label class="form-check-label" for="super-admin"
+                          >Super Admin</label
+                        >
+                      </div>
+                      <div class="form-check form-check-inline">
+                        <input
+                          class="form-check-input"
+                          type="radio"
+                          name="inlineRadioOptions2"
+                          id="team"
+                          value="team"
+                        />
+                        <label class="form-check-label" for="team"
+                          >Team</label
+                        >
+                      </div>
+                      <div class="form-check form-check-inline">
+                        <input
+                          class="form-check-input"
+                          type="radio"
+                          name="inlineRadioOptions2"
+                          id="user"
+                          value="user"
+                        />
+                        <label class="form-check-label" for="user"
+                          >User</label
+                        >
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="row mb-4">
+                  <div class="col-lg-3">
+                    <label for="address" class="form-label">Address</label>
+                  </div>
+                  <div class="col-lg-9">
+                    <textarea class="form-control" id="address" rows="3" placeholder="Enter your address"></textarea>
+                  </div>
+                </div>
+
+                <div class="row mb-4">
+                  <div class="col-lg-3">
+                    <label for="city" class="form-label"
+                      >City</label
+                    >
+                  </div>
+                  <div class="col-lg-9">
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="city"
+                      placeholder="Enter City"
+                    />
+                  </div>
+                </div>
+
+                <div class="row mb-4">
+                  <div class="col-lg-3">
+                    <label for="state" class="form-label"
+                      >State</label
+                    >
+                  </div>
+                  <div class="col-lg-9">
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="state"
+                      placeholder="Enter State"
+                    />
+                  </div>
+                </div>
+
+                <div class="row mb-4">
+                  <div class="col-lg-3">
+                    <label for="zipcode" class="form-label"
+                      >Zip</label
+                    >
+                  </div>
+                  <div class="col-lg-9">
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="zipcode"
+                      placeholder="Enter Zip"
+                    />
+                  </div>
+                </div>
+                <div class="row mb-4">
+                  <div class="col-lg-3">
+                    <label for="country" class="form-label"
+                      >Country</label
+                    >
+                  </div>
+                  <div class="col-lg-9">
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="country"
+                      placeholder="Enter Country"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="text-end"><button type="submit" class="btn btn-primary"> Add User </button></div>
+          </div>
+          <!-- end card-body -->
+        </div>
+        <!-- end card -->
+      </div>
+      <!-- end col -->
+
+      <!-- end col -->
+    </div>
+
+    <!--end row-->
+  </Layout>
+</template>
---
+++ src/views/pages/user/Index.vue
@@ -0,0 +1,124 @@
+<script>
+import Layout from "../../../layouts/main.vue";
+import PageHeader from "@/components/page-header";
+import Multiselect from "@vueform/multiselect";
+import "@vueform/multiselect/themes/default.css";
+
+import "prismjs";
+import "prismjs/themes/prism.css";
+
+export default {
+  data() {
+    return {
+      title: "Users",
+      selectedCountry: "india",
+    };
+  },
+  components: {
+    Layout,
+    PageHeader,
+    Multiselect,
+  },
+  mounted() {},
+};
+</script>
+
+<template>
+  <Layout>
+    <PageHeader :title="title" />
+    <div class="row">
+      <div class="col-xl-12">
+        <div class="filter">
+          <div class="row">
+            <div class="col-3">
+              <Multiselect
+                v-model="selectedCountry"
+                :close-on-select="true"
+                :searchable="true"
+                :create-option="true"
+                :options="[
+                  { value: 'india', label: 'India' },
+                  { value: 'arabic', label: 'Arabic' },
+                ]"
+              />
+            </div>
+            <div class="col-9 d-flex justify-content-end">
+              <b-button
+                variant="primary"
+                class="btn btn-primary waves-effect waves-light"
+                >Filter</b-button
+              >
+            </div>
+          </div>
+        </div>
+
+        <div class="card mt-4">
+          <div class="card-header align-items-center d-flex">
+            <h4 class="card-title mb-0 flex-grow-1">Users</h4>
+            <div class="flex-shrink-0">
+              <router-link
+                to="/users/add"
+                class="btn btn-primary waves-effect waves-light"
+              >
+                Add User
+              </router-link>
+            </div>
+          </div>
+          <!-- end card header -->
+
+          <div class="card-body">
+            <div class="table-responsive">
+              <table class="table align-middle table-nowrap mb-0">
+                <thead>
+                  <tr>
+                    <th scope="col">Date</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Phone</th>
+                    <th scope="col">Country</th>
+                    <th scope="col">Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>01-01-22</td>
+                    <td>Bobby Davis</td>
+                    <td>bobbydavis@gmail.com</td>
+                    <td>7894561230</td>
+                    <td>India</td>
+                    <td>
+                      <a href="javascript:void(0);" class="link-success"
+                        >View Details
+                        <i class="ri-arrow-right-line align-middle"></i
+                      ></a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>02-01-22</td>
+                    <td>Hello World</td>
+                    <td>helloworld@gmail.com</td>
+                    <td>7894561230</td>
+                    <td>India</td>
+                    <td>
+                      <a href="javascript:void(0);" class="link-success"
+                        >View Details
+                        <i class="ri-arrow-right-line align-middle"></i
+                      ></a>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <!-- end card-body -->
+        </div>
+        <!-- end card -->
+      </div>
+      <!-- end col -->
+
+      <!-- end col -->
+    </div>
+
+    <!--end row-->
+  </Layout>
+</template>
